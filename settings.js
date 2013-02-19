cd247fa0fb98abfc46a4ed6b7e66490721d3ec32
Fabricio Tavares de Oliveira <fabriciotavareso@gmail.com>
2013-02-19 11:52:06 -0300

    Update FIXTURES

--- settings.js
+++ settings.js
@@ -1,5 +1,84 @@
+var app, express, FIXTURES, querystring, BufferJoiner;
+
+// ..........................................................
+// NODE MODULES
+//
+express = require('express');
+querystring = require('querystring'),
+BufferJoiner = require('bufferjoiner');
+
+// ..........................................................
+// FIXTURES
+//
+FIXTURES = {
+    people: [
+        {
+            "id": 1,
+            "first_name": 'Fabricio',
+            "last_name": 'Tavares',
+            "twitter": 'fabriciotav',
+            "frameworks": [
+                {
+                    id: 1,
+                    name: 'Ember'
+                },
+                {
+                    id: 3,
+                    name:'Backbone'
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "first_name": 'Tiago',
+            "last_name": 'Cussiol',
+            "twitter": '',
+            "frameworks": []
+        },
+        {
+            "id": 3,
+            "first_name": 'André',
+            "last_name": '',
+            "twitter": '',
+            "frameworks": []
+        }
+    ],
+
+    frameworks: [
+        {
+            id: 1,
+            name: 'Ember.js'
+        },
+        {
+            id: 2,
+            name: 'AngularJS'
+        },
+        {
+            id: 3,
+            name: 'Backbone.js'
+        },
+        {
+            id: 4,
+            name: 'Dojo'
+        },
+        {
+            id: 5,
+            name: 'KnockoutJS'
+        },
+        {
+            id: 6,
+            name: 'YUI'
+        }
+    ]
+};
+
+// app = express(); // localhost
+
+// ..........................................................
+// EXPORTS
+//
 exports.routes = function(app) {
-    app.all('*', function(req, res, next){
+    app.all('*', function(req, res, next) {
         if (!req.get('Origin')) return next();
         // use "*" here to accept any origin
         res.set('Access-Control-Allow-Origin', '*');
@@ -10,53 +89,101 @@ exports.routes = function(app) {
         next();
     });
 
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
 
-    app.get('/contacts', function(req, res) {
-        var contacts;
+    // FIND
+    app.get('/people', function(req, res) {
+        var people;
+        people = FIXTURES.people;
 
-        contacts = [
-            {
-                id: 1,
-                firstName: 'Fabricio',
-                lastName: 'Tavares',
-                emails: [1, 2]
-            },
-            {
-                id: 2,
-                firstName: 'Tiago',
-                lastName: 'Cussiol',
-                emails: [3]
-            },
-            {
-                id: 3,
-                firstName: 'André',
-                lastName: '',
-                emails: []
-            }
-        ];
-        res.send({ contacts: contacts });
+        res.send({ "people": people });
     });
 
-    app.get('/emails', function(req, res) {
-        var emails;
+    // FIND ONE
+    app.get('/people/:id', function(req, res) {
+        var contact, reqId;
 
-        emails = [
-            {
-                id: 1,
-                contact_id: 1,
-                email: 'fabricio1@email.com'
-            },
-            {
-                id: 2,
-                contact_id: 1,
-                email: 'fabricio2@email.com'
-            },
-            {
-                id: 3,
-                contact_id: 2,
-                email: 'tiago@email.com'
+        reqId = req.params.id;
+
+        FIXTURES.people.forEach(function(d, i) {
+            if (d.id == reqId) {
+                return res.send({ person: d });
             }
-        ];
-        res.send({ emails: emails });
+        });
     });
+
+    // CREATE
+    app.post('/people', function(req, res) {
+        var person, reqPaylod, rawBody;
+
+        rawBody = new BufferJoiner();
+
+        req.on('data', function (chunk) {
+          rawBody.add(chunk);
+        });
+
+        req.once('end', function () {
+            rawBody = rawBody.join();
+
+            reqPaylod = JSON.parse(rawBody.toString('utf8'));
+            console.log( "reqPaylod", reqPaylod );
+            
+            person = {
+                  id: FIXTURES.people.length + 1,
+                  first_name: reqPaylod.person.first_name,
+                  last_name: reqPaylod.person.last_name,
+                  twitter: reqPaylod.person.twitter,
+                  frameworks: reqPaylod.person.frameworks
+              };
+      
+              FIXTURES.people.push(person);
+      
+              return res.send({ person: person });
+        });        
+    });
+
+    // UPDATE
+    app.put('/people/:id', function(req, res) {
+        var person;
+
+        return res.send({ person: person });
+    });
+
+    // DELETE
+    app.delete('/people/:id', function(req, res) {
+        var contact, reqId;
+
+        reqId = req.params.id;
+
+        FIXTURES.people.forEach(function(d, i) {
+            if (d.id == reqId) {
+                FIXTURES.people.splice(i, 1);
+                return res.send(200);
+            }
+        });
+    });
+
+    // Frameworks
+    // FIND
+    app.get('/frameworks', function(req, res) {
+        var frameworks;
+        frameworks = FIXTURES.frameworks;
+        res.send({ "frameworks": frameworks })
+    });
+
+    // FIND ONE
+    app.get('/frameworks/:id', function(req, res) {
+        var contact, reqId;
+
+        reqId = req.params.id;
+
+        FIXTURES.frameworks.forEach(function(d, i) {
+            if (d.id == reqId) {
+                return res.send({ framework: d });
+            }
+        });
+    });
+
+    // app.listen(4000); // localhost
 };
